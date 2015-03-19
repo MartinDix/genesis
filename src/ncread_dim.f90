1d06c6defe897b12361d5bf44359f29065e9e8e0
Holger Wolff <holger.wolff@monash.edu>
2015-03-19 19:04:57 +1100

    Second minor bug causing a segfault

--- src/ncread_dim.f90
+++ src/ncread_dim.f90
@@ -105,7 +105,8 @@ subroutine ncread_dim(ncid,nvar,nlon,nlat,nlev,nrec,  &
   dd = (var_ndims == 3)
   if (dd) then
     var_dimids(4) = var_dimids(3)
-    var_dimids(3) = -1000
+    var_dimids(3) = var_dimids(2)
+    var_dimids(2) = -1000
   end if
   rec_varid = var_dimids(1)
   lvl_varid = var_dimids(2)
